f9c2d9f44aadad582fd641b1bd8f788678dd93ff
Jason <jason.chen0301@gmail.com>
2021-08-02 11:56:09 +0800

    remove debug

---
+++ Debug/sources.mk
@@ -0,0 +1,53 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+OBJ_SRCS := 
+S_SRCS := 
+ASM_SRCS := 
+C_SRCS := 
+S_UPPER_SRCS := 
+O_SRCS := 
+EXECUTABLES := 
+OBJS := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+MIMXRT1062 \
+board \
+component/lists \
+component/osa \
+device \
+drivers \
+source/autobaud \
+source/bootloader/src \
+source/crc \
+source/drivers/dcp \
+source/drivers/flexspi \
+source/drivers/flexspi_nand \
+source/drivers/flexspi_nor \
+source/drivers/microseconds \
+source/drivers/nand_ecc \
+source/drivers/ocotp \
+source/drivers/sdmmc \
+source/drivers/semc \
+source/drivers/semc_nand \
+source/drivers/semc_nor \
+source/drivers/serial_nor_eeprom \
+source/drivers/trng \
+source/drivers/usdhc \
+source/memory/src \
+source/packet/src \
+source/property/src \
+source/sbloader/src \
+source \
+source/usb/bm_composite \
+source/utilities/src \
+startup \
+usb/device/class/hid \
+usb/device/class \
+usb/device/source/ehci \
+usb/device/source \
+usb/phy \
+
